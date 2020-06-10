b14f8da4fe13dceca2231f5c66fec1c8f21827a9
damiantomczyk32 <damiantomczyk32@gmail.com>
2020-06-10 20:12:48 +0200

    New file

---
+++ ConsoleApp_3.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pierwszy prgram po długiej przewie \nWraz z przećwiczneiem komentarzy SZMATO!");
+
+            /*Komentarz blokowy Szmato!*/
+            //Komentarz Liniowy
+            /// <summary>Komentarz XML</summary>
+            Console.ReadKey();
+        }
+    }
+}
