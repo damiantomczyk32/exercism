f913a4be3783324c62bbecad4bc07e19320476d0
damiantomczyk32 <damiantomczyk32@gmail.com>
2020-06-13 11:02:07 +0200

    Add new program

---
+++ CollatzConjecture/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollatzConjecture
+{
+    public static class CollatzConjecture
+    {
+        public static int Steps(int number)
+        {
+            if (number < 0) throw new ArgumentException();
+            if (number == 1) return 0;
+            if (number % 2 == 0) return 1 + Steps(number / 2);
+            return 1 + Steps(number * 3 + 1);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CollatzConjecture.Steps(16));
+            Console.ReadKey();
+        }
+    }
+}
