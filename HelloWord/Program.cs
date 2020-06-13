0a48fce18f5ad071b489ef5bb59655080ff4a558
damiantomczyk32 <damiantomczyk32@gmail.com>
2020-06-13 10:56:05 +0200

    Add new program

---
+++ HelloWord/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    class HelloWord
+    {
+        public static string SayHello(string x="you")
+        {
+            return $"One for { x }, one for me.";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HelloWord.SayHello("BICZ"));
+            Console.ReadKey();
+        }
+    }
+}
