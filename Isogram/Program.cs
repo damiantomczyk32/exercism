d276294d8259791f0d90392e1da469393ad3dafd
damiantomczyk32 <damiantomczyk32@gmail.com>
2020-06-14 20:13:06 +0200

    Add new file

---
+++ Isogram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isogram
+{
+    public class Isogram
+    {
+        public static bool CheckIsogram(string word)
+        {
+            char[] arr = word.ToLower().ToArray();
+            Array.Sort(arr);
+            for (int i = 0; i < word.Length - 1; i++)
+            {
+                if (arr[i].ToString() == " " || arr[i].ToString() == "-")
+                    continue;
+                if (arr[i] == arr[i + 1])
+                    return false;
+            }
+            return true;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Isogram.CheckIsogram("Emily Jung Schwartzkopf"));
+            Console.ReadKey();
+        }
+    }
+}
