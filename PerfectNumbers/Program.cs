f4bf81342cd8b2b6d680aae9e676c6c9520eb418
damiantomczyk32 <damiantomczyk32@gmail.com>
2020-06-14 20:18:00 +0200

    Add new file

---
+++ PerfectNumbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_Numbers
+{
+    public enum Classification
+    {
+        Perfect,
+        Abundant,
+        Deficient
+    }
+
+    public static class PerfectNumbers
+    {
+        public static Classification Classify(int number)
+        {
+            int sum = 0;
+            if (number <= 0)
+            {
+                throw new ArgumentOutOfRangeException("wrong value");
+            }
+            for (int i = 1; i<number;i++)
+            {
+                if (number % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            if (sum == number) { return Classification.Perfect; }
+            if (sum > number) { return Classification.Abundant; }
+            if (sum < number) { return Classification.Deficient; }
+            return (Classification)number;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = (Classification)1;
+
+            //Console.WriteLine(a);
+            Console.WriteLine(PerfectNumbers.Classify(12));
+            Console.ReadKey();
+        }
+    }
+}
