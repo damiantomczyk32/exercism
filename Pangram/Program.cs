5b1e3a5f9100e3fa66f67e43de637a0b7202e01d
damiantomczyk32 <damiantomczyk32@gmail.com>
2020-06-14 20:16:09 +0200

    Add new file

---
+++ Pangram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pangram
+{
+    public static class Pangram
+    {
+        static char[] alphabet = new[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        public static bool IsPangram(string input)
+        {
+            if(string.IsNullOrWhiteSpace(input))
+            return false;
+
+            var smallInput = input.ToLower();
+            return alphabet.All(c=> smallInput.Contains(c)  );
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Pangram.IsPangram("abcdefghijklmnopqrstuvwxyz"));
+            Console.ReadKey();
+        }
+    }
+}
